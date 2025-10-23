541b65c9dbd6c2e01acb869169d51f0e25c3173e
Vaishnavi-Gattagoni <vaishnavig860@gmail.com>
2025-10-23 12:21:40 +0530

    Update repository configuration for Vaishnavi-Portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/vaishnaviworks/' : '/',
+  base: mode === 'production' ? '/Vaishnavi-Portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
